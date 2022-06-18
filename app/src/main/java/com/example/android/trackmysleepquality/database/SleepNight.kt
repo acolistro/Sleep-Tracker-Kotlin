d11e82d09f9cd37b1fdd290df9fcfe9911e79739
acolistro <arcolistro@gmail.com>
2022-06-17 22:26:14 -0700

    Add sleep night data class and annotate it to assign name to the table and columns

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -16,3 +16,23 @@
 
 package com.example.android.trackmysleepquality.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Denotes a table being created with the name daily_sleep_quality_table
+ */
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight(
+    //Indicates which parameter is primary key and tells Room to auto-generate a unique id for each night
+    @PrimaryKey(autoGenerate = true)
+    var nightId: Long = 0L,
+    //indicates that this is a column and assigns a name to the column
+    @ColumnInfo(name = "start_time_milli")
+    val startTimeMilli: Long = System.currentTimeMillis(),
+    @ColumnInfo(name = "end_time_milli")
+    var endTimeMilli: Long = startTimeMilli,
+    @ColumnInfo(name = "quality_rating")
+    var sleepQuality: Int = -1
+)
